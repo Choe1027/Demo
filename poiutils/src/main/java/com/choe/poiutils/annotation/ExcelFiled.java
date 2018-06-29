4e25b4ab980c3b4034a401f8f4e067d3e345c724
cyk <348863046@qq.com>
2018-06-29 17:58:18 +0800

    创建注解

---
+++ poiutils/src/main/java/com/choe/poiutils/annotation/ExcelFiled.java
@@ -0,0 +1,28 @@
+package com.choe.poiutils.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author cyk
+ * @date 2018/6/29/029 17:51
+ * @email choe0227@163.com
+ * @desc
+ * @modifier
+ * @modify_time
+ * @modify_remark
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface ExcelFiled {
+    /**表格列名*/
+    String colName() default "";
+    /**表格宽度*/
+    int colWidth() default 4000;
+
+    int colIndex() default 1;
+}
