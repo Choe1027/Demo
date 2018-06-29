84b7e2f1a0167ccf1a2617e645498ad01a1480d6
cyk <348863046@qq.com>
2018-06-29 17:50:36 +0800

    poi工具类创建项目

---
+++ poiutils/src/main/java/com/choe/poiutils/PoiutilsApplication.java
@@ -0,0 +1,12 @@
+package com.choe.poiutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoiutilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoiutilsApplication.class, args);
+    }
+}
---
+++ poiutils/src/test/java/com/choe/poiutils/PoiutilsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.choe.poiutils;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PoiutilsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
