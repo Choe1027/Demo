2f8b2d821522c59939d6f74140bce9d424ee751b
cyk <348863046@qq.com>
2018-07-05 18:04:44 +0800

    springboot 测试

---
+++ springboottest/src/main/java/com/choe/springboottest/config/xml/XmlSettings.java
@@ -0,0 +1,17 @@
+package com.choe.springboottest.config.xml;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author cyk
+ * @date 2018/7/5/005 17:07
+ * @email choe0227@163.com
+ * @desc
+ * @modifier
+ * @modify_time
+ * @modify_remark
+ */
+@Component
+//@ImportResource("classpath:test.xml")
+public class XmlSettings {
+}
---
+++ springboottest/src/test/java/com/choe/springboottest/SpringboottestApplicationTests.java
@@ -0,0 +1,24 @@
+package com.choe.springboottest;
+
+import com.choe.springboottest.config.property.ChoeSettings;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringboottestApplicationTests {
+
+    @Autowired
+    private ChoeSettings choeSettings;
+
+
+    @Test
+    public void contextLoads() {
+        System.out.println("name "+choeSettings.getName() + "+===age ="+ choeSettings.getAge() +"==address="+choeSettings.getAddress());
+    }
+
+}
