2dda6ae57e32bdbca415b3fbf845ecddf76056dc
cyk <348863046@qq.com>
2018-07-06 18:42:00 +0800

    去掉没用东西

--- mysql/src/main/java/com/choe/mysql/MysqlApplication.java
+++ mysql/src/main/java/com/choe/mysql/MysqlApplication.java
@@ -1,9 +1,6 @@
 package com.choe.mysql;
 
-import com.example.shyFly.easySql.EasySqlExecution;
-
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
@@ -12,19 +9,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @MapperScan(basePackages = {"com.choe.mysql.dao"})
 public class MysqlApplication {
-    @Autowired
-    private EasySqlExecution easySqlExecution;
 
 
     public static void main(String[] args) {
         SpringApplication.run(MysqlApplication.class, args);
     }
 
-//    @PostConstruct
-//    private void testImport(){
-//        Student2 s = new Student2();
-//        List<Student2> select = easySqlExecution.select(s);
-//        System.out.println(select.size());
-//
-//    }
 }
