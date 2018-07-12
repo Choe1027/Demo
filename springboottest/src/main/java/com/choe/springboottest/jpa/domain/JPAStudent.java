79ddc5822109ce141a894ad02c2aeece83e859a6
cyk <348863046@qq.com>
2018-07-12 17:40:49 +0800

    引入JPA

---
+++ springboottest/src/main/java/com/choe/springboottest/jpa/domain/JPAStudent.java
@@ -0,0 +1,15 @@
+package com.choe.springboottest.jpa.domain;
+
+/**
+ * @author cyk
+ * @date 2018/7/12/012 17:36
+ * @email choe0227@163.com
+ * @desc  通过JPA的方式来操作数据库
+ * @modifier
+ * @modify_time
+ * @modify_remark
+ */
+public class JPAStudent {
+
+
+}
---
+++ springboottest/src/main/java/com/choe/springboottest/jpa/repositorys/JpaStudentRepository.java
@@ -0,0 +1,13 @@
+package com.choe.springboottest.jpa.repositorys;
+
+/**
+ * @author cyk
+ * @date 2018/7/12/012 17:40
+ * @email choe0227@163.com
+ * @desc
+ * @modifier
+ * @modify_time
+ * @modify_remark
+ */
+public interface JpaStudentRepository {
+}
