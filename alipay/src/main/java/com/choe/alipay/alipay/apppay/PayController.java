74bfae0da00d6d2fdff0589817bd939b998b2700
cyk <348863046@qq.com>
2018-07-20 17:12:26 +0800

    支付宝支付

---
+++ alipay/src/main/java/com/choe/alipay/alipay/apppay/PayController.java
@@ -0,0 +1,24 @@
+package com.choe.alipay.alipay.apppay;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author cyk
+ * @date 2018/7/19/019 13:40
+ * @email choe0227@163.com
+ * @desc
+ * @modifier
+ * @modify_time
+ * @modify_remark
+ */
+@RequestMapping()
+public class PayController {
+
+
+
+    public void test(){
+
+
+    }
+
+}
